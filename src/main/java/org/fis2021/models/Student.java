52d7f419541713c92e1f3297ce848a8099e4bf61
ghecatalina <catalina2001>
2021-04-15 18:42:21 +0300

    Create Database File

---
+++ src/main/java/org/fis2021/models/Student.java
@@ -0,0 +1,62 @@
+package org.fis2021.models;
+
+public class Student {
+    private String nume, facultate, specializare, nrMatricol, username, parola;
+
+    public Student(String nume, String facultate, String specializare, String nrMatricol, String username, String parola) {
+        this.nume = nume;
+        this.facultate = facultate;
+        this.specializare = specializare;
+        this.nrMatricol = nrMatricol;
+        this.username = username;
+        this.parola = parola;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getFacultate() {
+        return facultate;
+    }
+
+    public void setFacultate(String facultate) {
+        this.facultate = facultate;
+    }
+
+    public String getSpecializare() {
+        return specializare;
+    }
+
+    public void setSpecializare(String specializare) {
+        this.specializare = specializare;
+    }
+
+    public String getNrMatricol() {
+        return nrMatricol;
+    }
+
+    public void setNrMatricol(String nrMatricol) {
+        this.nrMatricol = nrMatricol;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+}
---
+++ src/main/java/org/fis2021/models/Tutor.java
@@ -0,0 +1,53 @@
+package org.fis2021.models;
+
+public class Tutor {
+    private String nume, username, parola, materie, specializare ;
+
+    public Tutor(String nume, String username, String parola, String materie, String specializare) {
+        this.nume = nume;
+        this.username = username;
+        this.parola = parola;
+        this.materie = materie;
+        this.specializare = specializare;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+
+    public String getMaterie() {
+        return materie;
+    }
+
+    public void setMaterie(String materie) {
+        this.materie = materie;
+    }
+
+    public String getSpecializare() {
+        return specializare;
+    }
+
+    public void setSpecializare(String specializare) {
+        this.specializare = specializare;
+    }
+}
