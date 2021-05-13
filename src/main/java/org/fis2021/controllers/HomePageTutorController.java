e794db91f22cc595441ab173f0b41ba6ab18c3e0
WaganahaGouki <casian.suli@student.upt.ro>
2021-05-13 16:17:16 +0300

    Tutors can add new courses Courses are displayed in the Calendar

--- src/main/java/org/fis2021/controllers/HomePageTutorController.java
+++ src/main/java/org/fis2021/controllers/HomePageTutorController.java
@@ -115,4 +115,12 @@ public class HomePageTutorController implements Initializable {
         stage.setTitle("Tutor Searching App - Calendar");
         stage.setScene(scene);
     }
+
+    @FXML
+    void switchToCourses() throws IOException {
+        Stage stage = (Stage) gridPane.getScene().getWindow();
+        Scene scene = new Scene(loadFXML("addCourses"), 1280, 720);
+        stage.setTitle("Tutor Searching App - Add Courses");
+        stage.setScene(scene);
+    }
 }
--- src/main/java/org/fis2021/controllers/RequestTutorController.java
+++ src/main/java/org/fis2021/controllers/RequestTutorController.java
@@ -148,4 +148,12 @@ public class RequestTutorController implements Initializable {
         stage.setScene(scene);
     }
 
+    @FXML
+    void switchToCourses() throws IOException {
+        Stage stage = (Stage) scrollPanel.getScene().getWindow();
+        Scene scene = new Scene(loadFXML("addCourses"), 1280, 720);
+        stage.setTitle("Tutor Searching App - Add Courses");
+        stage.setScene(scene);
+    }
+
 }
